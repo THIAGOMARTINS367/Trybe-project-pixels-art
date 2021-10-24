d92fa680ac82fe950b0604deab828470bf2e46ab
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2021-10-24 20:43:20 -0300

    Requisitos de 6 a 8 feitos.

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
 let pixelBoard = document.querySelector('#pixel-board');
+let liColor = document.querySelectorAll('.color');
 
-// document.onload = function() {
-//   for (let index = 1; index <= 25; index += 1) {
-//     pixelBoard.innerHTML = "<li class='pixel'> </li>";
-    
-//   }
-// }
+liColor[0].style.backgroundColor = 'black';
+liColor[1].style.backgroundColor = 'blue';
+liColor[2].style.backgroundColor = 'red';
+liColor[3].style.backgroundColor = 'rgb(0, 226, 0)';
 
 for (let index = 1; index <= 25; index += 1) {
   pixelBoard.innerHTML += "<li class='pixel'> </li>";
   
 }
+
+let color = 'black';
+function selectColor(event) {
+  let selectedElement = event.target;
+  for (const iterator of selectedElement.classList) {
+    if (iterator === 'selected') {
+    
+    } else {
+      for (const key in liColor) {
+        liColor[key].className = 'color';
+      }
+
+      event.target.classList.add('selected');
+      color = event.target.style.backgroundColor;
+    }
+  }
+  
+}
+
+function colorPixels(event) {
+  event.target.style.backgroundColor = color;
+  
+}
+
+document.addEventListener('click', function(event) {
+  let classList = event.target.classList;
+  for (const iterator of classList) {
+    if (iterator === 'color') {
+      selectColor(event)
+    };
+
+  }
+  
+});
+
+document.addEventListener('click', function(event) {
+  let classList = event.target.classList;
+  for (const iterator of classList) {
+    if (iterator === 'pixel') {
+      colorPixels(event)
+    };
+
+  }
+  
+});
